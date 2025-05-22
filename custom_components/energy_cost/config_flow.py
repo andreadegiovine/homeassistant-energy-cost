29ad53cd7828360c7bc0cc2013139ae27be2394a
andreadegiovine <68383596+andreadegiovine@users.noreply.github.com>
2025-05-22 17:11:58 +0200

    reconfigure entry

--- custom_components/energy_cost/config_flow.py
+++ custom_components/energy_cost/config_flow.py
@@ -2,8 +2,6 @@ from homeassistant import config_entries
 import voluptuous as vol
 from homeassistant.helpers.selector import selector
 import logging
-import string
-import random
 from .const import (
                         DOMAIN,
                         FIELD_NAME,
@@ -25,18 +23,77 @@ from .const import (
 
 _LOGGER = logging.getLogger(__name__)
 
-DATA_SCHEMA_1 = vol.Schema({
-    vol.Required(FIELD_NAME, default="Home"): str,
-    vol.Required(FIELD_POWER_ENTITY): selector({ "entity": { "device_class": "energy" } }),
-    vol.Required(FIELD_POWER, default=4.5): vol.Coerce(float),
-    vol.Required(FIELD_RATE_MODE, default=FIELD_RATE_MODE_FLEX): selector({ "select": { "options": [FIELD_RATE_MODE_MONO, FIELD_RATE_MODE_FLEX], "mode": "dropdown", "translation_key": FIELD_RATE_MODE } }),
-    vol.Required(FIELD_PUN_MODE, default=True): selector({ "boolean": {} })
-})
+def DATA_SCHEMA_1(reconfig=None):
+    defaults = {
+        FIELD_NAME: "Home",
+        FIELD_POWER_ENTITY: None,
+        FIELD_POWER: 4.5,
+        FIELD_RATE_MODE: FIELD_RATE_MODE_FLEX,
+        FIELD_PUN_MODE: True
+    }
 
-DATA_SCHEMA_2 = vol.Schema({
-    vol.Optional(FIELD_FIXED_FEE, default=6.50): vol.Coerce(float),
-    vol.Required(FIELD_VAT_FEE, default=10): vol.Coerce(float),
-})
+    step_schema = vol.Schema({})
+    if reconfig:
+        defaults = reconfig
+    else:
+        step_schema = step_schema.extend({vol.Required(FIELD_NAME, default=defaults[FIELD_NAME]): str})
+
+    return step_schema.extend({
+        vol.Required(FIELD_POWER_ENTITY, default=defaults[FIELD_POWER_ENTITY]): selector({"entity": {"device_class": "energy"}}),
+        vol.Required(FIELD_POWER, default=defaults[FIELD_POWER]): vol.Coerce(float),
+        vol.Required(FIELD_RATE_MODE, default=defaults[FIELD_RATE_MODE]): selector({"select": {
+            "options": [FIELD_RATE_MODE_MONO, FIELD_RATE_MODE_FLEX], "mode": "dropdown",
+            "translation_key": FIELD_RATE_MODE}}),
+        vol.Required(FIELD_PUN_MODE, default=defaults[FIELD_PUN_MODE]): selector({"boolean": {}})
+    })
+
+def DATA_SCHEMA_2(reconfig=None):
+    defaults = {
+        FIELD_FIXED_FEE: 6.50,
+        FIELD_VAT_FEE: 10
+    }
+    if reconfig:
+        defaults = reconfig
+    return vol.Schema({
+        vol.Optional(FIELD_FIXED_FEE, default=defaults[FIELD_FIXED_FEE]): vol.Coerce(float),
+        vol.Required(FIELD_VAT_FEE, default=defaults[FIELD_VAT_FEE]): vol.Coerce(float),
+    })
+
+def DATA_SCHEMA_3(reconfig=None):
+    defaults = {
+        FIELD_PUN_ENTITY: None
+    }
+    if reconfig:
+        defaults = reconfig
+    return {
+        vol.Required(FIELD_PUN_ENTITY, default=defaults[FIELD_PUN_ENTITY]): selector({ "entity": { "integration": "pun_sensor", "domain": "sensor" } })
+    }
+
+def DATA_SCHEMA_4(reconfig=None):
+    defaults = {
+        FIELD_MONO_RATE: 0.01328
+    }
+    if reconfig:
+        defaults = reconfig
+    return {
+        vol.Required(FIELD_MONO_RATE, default=defaults[FIELD_MONO_RATE]): vol.Coerce(float)
+    }
+
+def DATA_SCHEMA_5(reconfig=None):
+    defaults = {
+        FIELD_CURRENT_RATE_ENTITY: None,
+        FIELD_F1_RATE: 0.01328,
+        FIELD_F2_RATE: 0.01328,
+        FIELD_F3_RATE: 0.01328,
+    }
+    if reconfig:
+        defaults = reconfig
+    return {
+        vol.Required(FIELD_CURRENT_RATE_ENTITY, default=defaults[FIELD_CURRENT_RATE_ENTITY]): selector({ "entity": { "device_class": "enum", "integration": "pun_sensor", "domain": "sensor" } }),
+        vol.Required(FIELD_F1_RATE, default=defaults[FIELD_F1_RATE]): vol.Coerce(float),
+        vol.Required(FIELD_F2_RATE, default=defaults[FIELD_F2_RATE]): vol.Coerce(float),
+        vol.Required(FIELD_F3_RATE, default=defaults[FIELD_F3_RATE]): vol.Coerce(float),
+    }
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -49,24 +106,34 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     def __init__(self):
         """Initialize config flow."""
         self.data = None
+        self.reconfig = None
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
 
         if user_input is not None:
             self.data = user_input
-            unique_id = res = ''.join(random.choices(string.ascii_letters, k=10))
+
+            if self.source == config_entries.SOURCE_RECONFIGURE:
+                unique_id = self.reconfig[FIELD_NAME].lower()
+            else:
+                unique_id = self.data[FIELD_NAME].lower()
+
             await self.async_set_unique_id(unique_id)
+
+            if self.source != config_entries.SOURCE_RECONFIGURE:
+                self._abort_if_unique_id_configured()
+
             return await self.async_step_final()
 
         return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA_1
+            step_id="user", data_schema=DATA_SCHEMA_1(self.reconfig)
         )
 
     async def async_step_final(self, user_input=None):
         """Handle the final step."""
 
-        step_schema = DATA_SCHEMA_2
+        step_schema = DATA_SCHEMA_2(self.reconfig)
 
         if user_input is not None:
             if user_input[FIELD_FIXED_FEE] is not None:
@@ -74,25 +141,25 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 config_data.update(self.data)
                 config_data.update(user_input)
                 _LOGGER.info(config_data)
+                if self.source == config_entries.SOURCE_RECONFIGURE:
+                    return self.async_update_reload_and_abort(self._get_reconfigure_entry(),  data_updates=config_data)
                 return self.async_create_entry(title=config_data[FIELD_NAME], data=config_data)
 
         if self.data[FIELD_PUN_MODE]:
-            step_schema = step_schema.extend({
-                vol.Required(FIELD_PUN_ENTITY): selector({ "entity": { "integration": "pun_sensor", "domain": "sensor" } })
-            })
+            step_schema = step_schema.extend(DATA_SCHEMA_3(self.reconfig))
 
         if self.data[FIELD_RATE_MODE] == FIELD_RATE_MODE_MONO:
-            step_schema = step_schema.extend({
-                vol.Required(FIELD_MONO_RATE, default=0.01328): vol.Coerce(float)
-            })
+            step_schema = step_schema.extend(DATA_SCHEMA_4(self.reconfig))
         else:
-            step_schema = step_schema.extend({
-                vol.Required(FIELD_CURRENT_RATE_ENTITY): selector({ "entity": { "device_class": "enum", "integration": "pun_sensor", "domain": "sensor" } }),
-                vol.Required(FIELD_F1_RATE, default=0.01328): vol.Coerce(float),
-                vol.Required(FIELD_F2_RATE, default=0.01328): vol.Coerce(float),
-                vol.Required(FIELD_F3_RATE, default=0.01328): vol.Coerce(float),
-            })
+            step_schema = step_schema.extend(DATA_SCHEMA_5(self.reconfig))
 
         return self.async_show_form(
             step_id="final", data_schema=step_schema
+        )
+
+    async def async_step_reconfigure(self, user_input):
+        self.reconfig = self._get_reconfigure_entry().data
+        _LOGGER.info(self.reconfig)
+        return self.async_show_form(
+            step_id="user", data_schema=DATA_SCHEMA_1(self.reconfig)
         )
